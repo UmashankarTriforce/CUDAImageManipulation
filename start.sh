99380c57e0a3829673baf9f92a01473a9cdc395b
UmashankarTriforce <umashankarsivakumar@gmail.com>
2019-07-26 12:14:03 +0530

    Removed npm install

--- start.sh
+++ start.sh
@@ -1,13 +1,3 @@
-if [ ! -d "server/node_modules/" ];then
-    cd server
-    npm install 
-    cd ..
-fi
-if [ ! -d "app/node_modules" ];then
-    cd app
-    npm install 
-    cd ..
-fi
 cd docker
 docker-compose up --remove-orphans
 docker-compose stop
