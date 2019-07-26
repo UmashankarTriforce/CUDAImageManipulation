82532b56cacb880a21a2c600aeab2618bec21a35
UmashankarTriforce <umashankarsivakumar@gmail.com>
2019-07-26 10:24:49 +0530

    Changed middleware

--- server/index.js
+++ server/index.js
@@ -3,12 +3,18 @@ const fetch = require('node-fetch')
 const app = express()
  
 app.get('/', function (req, res) {
-
-    fetch('http://172.18.0.2/bench').then(res => res.json()).then(function(data) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        res.send(data)  
-      });
+  
+  fetch('http://172.18.0.2/bench',
+  {
+    method : 'POST',
+    headers: {"Content-Type": "application/json"},
+    body : JSON.stringify(req.query)
+  }).then(res => res.json()).then(function(data) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "X-Requested-With");
+      res.send(data)  
+    });
 })
+
  
 app.listen(80)
