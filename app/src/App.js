a4ce0b96be15f3407cdae9bcbe1e16eaa91c428b
UmashankarTriforce <umashankarsivakumar@gmail.com>
2019-07-26 10:24:16 +0530

    Changed front end

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import axios from 'axios'
-import logo from './logo.svg';
 import './App.css';
 
 
@@ -15,7 +14,13 @@ class App extends Component{
   }
 
   fetch_content(){
-    axios.get('http://192.168.1.4:8083/')
+    axios.get('http://192.168.1.4:8083/',{
+      params: {
+        m : 5000,
+        k : 10000,
+        n : 10000
+      }
+    })
     .then(response => {
           this.setState({
             cuda : "Single Precision - " + response.data["Single"] + " Double Precision - " + response.data["Double"]
@@ -26,18 +31,6 @@ class App extends Component{
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
           <div>
             {this.state.cuda}
           </div>
